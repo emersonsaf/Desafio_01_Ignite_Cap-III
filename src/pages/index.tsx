c8c87ef243a6f87a1bddc56fcc4d0156a34e5ade
emersonsaf <emersonfilho3@gmail.com>
2021-09-17 13:51:27 -0300

    add post by slug, and fix bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { RiCalendarLine } from 'react-icons/ri'
 import { MdPersonOutline } from 'react-icons/md'
 import Header from '../components/Header';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -32,29 +33,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ results, next_page }: PostPagination) {
-  console.log(results)
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState(postsPagination.results)
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+ 
+  async function handleNextPage() {
+    if (nextPage) {
+      await fetch(nextPage)
+        .then(response => response.json())
+        .then(data => {
+          setNextPage(data.next_page)
+          const newPosts = [...posts, data.results[0]]
+          setPosts(newPosts)
+          setNextPage(data.next_page)
+        })
+    }
+  }
+
   return (
     <>
       <main className={styles.container}>
         <div className={styles.posts}>
           <Header />
           {
-
-            results.map(post => (
-              <Link key={post.uid}  href={`/post/${post.uid}`}>
+            posts?.map(post => (
+              <Link key={post.uid} href={`/post/${post.uid}`}>
                 <a>
-                  <strong>{post.data.title}</strong>
-                  <p>{post.data.subtitle}</p>
+                  <strong>{post.data?.title}</strong>
+                  <p>{post.data?.subtitle}</p>
                   <div className={styles.info}>
-                    <time> <RiCalendarLine className={styles.logoTipo} /> {post.first_publication_date}</time>
-                    <span> <MdPersonOutline className={styles.logoTipo} /> {post.data.author}</span>
+                    <time> <RiCalendarLine className={styles.logoTipo} /> {post?.first_publication_date}</time>
+                    <span> <MdPersonOutline className={styles.logoTipo} /> {post.data?.author}</span>
                   </div>
                 </a>
               </Link>
             ))
           }
-          <a href='#' className={styles.link}> Carregar mais posts</a>
+          {
+            nextPage  ? <a href='#' onClick={handleNextPage} className={styles.link}> Carregar mais posts</a> : null
+          }
         </div>
       </main>
     </>
@@ -74,7 +91,10 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
+  // console.log(postsResponse)
+
   const results = postsResponse.results.map(post => {
+
     return {
       uid: post.uid,
       data: {
@@ -88,13 +108,16 @@ export const getStaticProps: GetStaticProps = async () => {
         {
           locale: ptBR,
         }
-      )
+      ),
     }
   })
 
   return {
     props: {
-      results,
+      postsPagination: {
+        results,
+        next_page: postsResponse.next_page,
+      }
     }
   }
 };
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,9 +3,16 @@ import { Head } from 'next/document';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { RichText } from 'prismic-dom';
+import { FaRegClock } from 'react-icons/fa'
+import { RiCalendarLine } from 'react-icons/ri';
+import { MdPersonOutline } from 'react-icons/md';
+import { parse } from 'path';
 
 interface Post {
   first_publication_date: string | null;
@@ -29,24 +36,78 @@ interface PostProps {
 }
 
 export default function PostPage({ post }: PostProps) {
+
+  const sizeWord = (post.data.content?.body.text).length
+
+  const timeToRead = ((((sizeWord / 10) * 60) / 200) / 60).toFixed();
+
   return (
     <>
-      <Header />
-      <h1>Testando Post</h1>
+      <main className={styles.container}>
+        <header>
+          <Header />
+        </header>
+        <div className={styles.postBody}>
+          <img src={post.data.banner.url}
+            alt="banner" />
+          <h1>{post.data.title}</h1>
+          <div className={styles.info}>
+            <time> <RiCalendarLine className={styles.logoTipo} /> {post.first_publication_date} </time>
+            <span> <MdPersonOutline className={styles.logoTipo} /> {post.data.author}</span>
+            <span> <FaRegClock className={styles.logoTipo} /> {timeToRead} min</span>
+          </div>
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.data.content.body?.text }} />
+        </div>
+      </main>
     </>
   )
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params
+  const prismic = getPrismicClient();
+  const postId = await prismic.getByUID('post', String(slug));
 
-//   // TODO
-// };
+  const content = postId.data.content[0]
+
+  const post = {
+    uid: postId.uid,
+    first_publication_date: format(
+      new Date(postId.first_publication_date),
+      "dd MMM yyyy",
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: postId.data.title,
+      banner: {
+        url: postId.data.banner.url
+      },
+      author: postId.data.author,
+      content: {
+        heading: postId.data.content[0].heading,
+        body: {
+          text: RichText.asHtml(postId.data.content[0].body)
+        }
+      }
+
+    },
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+};
